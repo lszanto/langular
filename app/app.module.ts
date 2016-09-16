2504cf50a8ebe1bdb45cbaf6a3b62dcc42f225b9
lszanto <luke.szanto@gmail.com>
2016-09-16 23:30:18 +1000

    Add fake memory serivce to fake an api

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -12,7 +16,9 @@ import { routing } from './app.routing';
 @NgModule({
     imports: [ 
         BrowserModule,
-        FormsModule ,
+        FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
         routing
     ],
     declarations: [ 
